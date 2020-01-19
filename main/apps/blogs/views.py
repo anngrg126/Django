f038a8dc3cab359345b1c1c40ac14bcc0adb752d
anngrg126 <anngrg126@gmail.com>
2020-01-19 17:38:08 +0530

    second app 'blogs' within main; home url host:8000/blogs

---
+++ main/apps/blogs/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect, HttpResponse
+
+# Create your views here.
+def index(request):
+    response = "placeholder to later display all the list of blogs"
+    return HttpResponse(response)
